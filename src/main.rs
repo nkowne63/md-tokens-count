c47dfc5fbc8d6f5c657982c6187d7b0a0ec52ab5
nkowne63rt <neutron63zf@gmail.com>
2024-07-22 23:51:32 +0900

    add: main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-pub mod counter;
+mod counter;
+use counter::recursive_count_tokens;
+use std::env::args;
 
 fn main() {
-    println!("Hello, world!");
+    // parse commandline args
+    let args: Vec<String> = args().collect();
+    let folder_path = &args[1];
+    let result = recursive_count_tokens(folder_path);
+    match result {
+        Ok(count) => println!("Total tokens: {}", count),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }

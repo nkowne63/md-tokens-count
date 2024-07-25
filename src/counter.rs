68f0a16ff7643c3e7cb1c77c292f2cd44d26c21d
nkowne63rt <neutron63zf@gmail.com>
2024-07-25 18:28:34 +0900

    add: gitignore filter

--- src/counter.rs
+++ src/counter.rs
@@ -1,35 +1,12 @@
 use anyhow::Result;
-use std::fs::{read_dir, read_to_string};
+use ignore::Walk;
+use std::fs::read_to_string;
 use std::path::Path;
 use tiktoken_rs::o200k_base;
 
 type StringPath = String;
 
-enum FolderItems {
-    File(StringPath),
-    Folder(StringPath),
-}
-
-fn ls(path: &StringPath) -> Result<Vec<FolderItems>> {
-    let entries = read_dir(path)?;
-    let folder_items: Vec<FolderItems> = entries
-        .map(|entry| {
-            let entry = entry.ok()?;
-            let path = entry.path();
-            let item_result = if path.is_dir() {
-                FolderItems::Folder(path.to_str()?.to_string())
-            } else {
-                FolderItems::File(path.to_str()?.to_string())
-            };
-            Some(item_result)
-        })
-        .filter_map(|item_result| match item_result {
-            Some(item) => Some(item),
-            None => None,
-        })
-        .collect();
-    return Ok(folder_items);
-}
+struct File(StringPath);
 
 fn is_md(path: &StringPath) -> bool {
     let path = Path::new(path);
@@ -44,24 +21,16 @@ fn read_txt(path: &StringPath) -> Result<String> {
     Ok(read_to_string(path)?)
 }
 
-fn recursive_ls(path: &StringPath) -> Result<Vec<FolderItems>> {
-    let current = ls(path)?;
-    let mut result = vec![];
-    for item in current {
-        match item {
-            FolderItems::File(file) => {
-                if is_md(&file) {
-                    result.push(FolderItems::File(file));
-                }
+fn recursive_ls_ignored(path: &StringPath) -> Result<Vec<File>> {
+    Ok(Walk::new(path)
+        .filter_map(|e| {
+            let e = e.ok()?;
+            if e.file_type()?.is_file() {
+                return Some(File(e.path().to_str()?.to_string()));
             }
-            FolderItems::Folder(folder) => {
-                result.push(FolderItems::Folder(folder.clone()));
-                let mut sub = recursive_ls(&folder)?;
-                result.append(&mut sub);
-            }
-        }
-    }
-    return Ok(result);
+            None
+        })
+        .collect::<Vec<_>>())
 }
 
 fn count_tokens(txt: &String) -> Result<usize> {
@@ -71,13 +40,10 @@ fn count_tokens(txt: &String) -> Result<usize> {
 }
 
 pub fn recursive_count_tokens(path: &StringPath) -> Result<usize> {
-    let files = recursive_ls(path)?;
+    let files = recursive_ls_ignored(path)?;
     let md_files: Vec<&String> = files
         .iter()
-        .filter_map(|item| match item {
-            FolderItems::File(path) if is_md(path) => Some(path),
-            _ => None,
-        })
+        .filter_map(|file| if is_md(&file.0) { Some(&file.0) } else { None })
         .collect();
     let txt_concat = md_files
         .iter()
